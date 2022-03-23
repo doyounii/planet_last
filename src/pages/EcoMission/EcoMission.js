00e099d85f6a0b933c44f65e555bff9e08f7e2c4
Soyoon Lee <lsyjyh@gmail.com>
2022-03-23 13:21:15 +0900

    merge Updating and master

--- src/pages/EcoMission/EcoMission.js
+++ src/pages/EcoMission/EcoMission.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import EcoStyle from './Eco.module.css';
 import HistorySample from '../../components/History/HistoryBackHome';
 import { FiShare } from 'react-icons/fi'
 
 function EcoMission() {
+    const [list, setList] = useState({});
+    const [loading, setloading] = useState(true);
+
+    const [todayMission, setTodayMission] = useState({});
+
+  useEffect(() => {
+    let isSubscribed = true;
+    fetch(
+      `/mission/yui12@gmail.com`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (isSubscribed) {
+          setTodayMission(data.todayMission);
+          setloading(false);
+        }
+      })
+      .catch((error) => {
+        console.log("error!");
+        console.log(error);
+      });
+    return () => (isSubscribed = false);
+  }, []);
+
+  const fetchFunc = () => {
+    //백엔드로 데이터 보내기
+    fetch(`/mission/yui12@gmail.com/${todayMission.emoji}/${todayMission.name}`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      'mission': todayMission.emoji,
+    })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if (response.token) {
+        localStorage.setItem('wtw-token', response.token);
+      }
+    })
+}
+
   return (
     <div className={EcoStyle.container}>
 
@@ -19,9 +72,10 @@ function EcoMission() {
         <div className={EcoStyle.mission_box}>
             <p>새로운 미션이 도착했어요!</p>
             <h1>오늘의 데일리 에코 미션</h1>
-            <h2>청구서 및 영수증을 온라인 발급받기</h2>
+            <h2>{todayMission.name}</h2>
+            {/*<p>{String.fromCodePoint(todayMission.emoji)}</p>*/}
             <img src="img/recipt.png" alt="recipt"></img>
-            <button className={EcoStyle.mission_btn}>달성</button>
+            <button type="submit" className={EcoStyle.mission_btn}>달성</button>
         </div>
         <div className={EcoStyle.sub_title}>
             <h1>10월 내가 달성한 미션</h1>
