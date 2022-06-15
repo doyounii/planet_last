186e1af04fcc74d2a7efb6548aaca9d93c5d7ce3
listenyoon <listenyoon@gmail.com>
2022-06-15 23:24:20 +0900

    mod CalendarPage fetch to axios

--- src/pages/Calendar/CalendarPage.js
+++ src/pages/Calendar/CalendarPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useQuery, useQueries, queryClient, useQueryClient } from "react-query";
+import { useQueries, useQueryClient } from "react-query";
 import { format, isSameMonth, subMonths, addMonths, parseISO } from "date-fns";
 import Footer from "../../components/Footer/Footer";
 import DateHeader from "../../components/DateHeader";
@@ -14,218 +14,28 @@ import { IoIosArrowForward } from "react-icons/io";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
 
 const fetchData = async (date) => {
-  // const response = await fetch(
-  //   `/calendar/${format(date, "yyyy")}/${format(date, "M")}`,
-  //   {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Accept: "application/json",
-  //     },
-  //   }
-  // );
-  // const data = await response.json();
-  const data = {
-    anniversaryList: ["2022-06-03", "2022-06-13", "2022-06-17"],
-    calendarDto: {
-      sumOfEcoCount: 29,
-      sumOfNoneEcoCount: 12,
-      totalMonthIncome: 0,
-      totalMonthExpenditure: 265277,
-      calendarDayDtos: [
-        {
-          date: "2022-06-20",
-          incomeDays: 0,
-          ecoCount: 1,
-          noneEcoCount: 1,
-          expenditureDays: 2000,
-        },
-        {
-          date: "2022-06-23",
-          incomeDays: 0,
-          ecoCount: 3,
-          noneEcoCount: 6,
-          expenditureDays: 19600,
-        },
-        {
-          date: "2022-06-24",
-          incomeDays: 0,
-          ecoCount: 4,
-          noneEcoCount: 2,
-          expenditureDays: 8400,
-        },
-        {
-          date: "2022-06-25",
-          incomeDays: 0,
-          ecoCount: 5,
-          noneEcoCount: 0,
-          expenditureDays: 7000,
-        },
-        {
-          date: "2022-06-26",
-          incomeDays: 0,
-          ecoCount: 16,
-          noneEcoCount: 3,
-          expenditureDays: 561872,
-        },
-      ],
-    },
-    content:
-      "자연과 가까울수록 병은 멀어지고, 자연과 멀수록 병은 가까워진다. - 요한 볼프강 폰 괴테",
-  };
+  const response = await axios.get(
+    `https://플랜잇.웹.한국:8080/calendar/${format(date, "yyyy")}/${format(
+      date,
+      "M"
+    )}`
+  );
+  const data = await response.data;
+
   return data;
 };
 
 const fetchDetailData = async (day) => {
-  // console.log("fetch");
-  // const date = parseISO(day);
-  // const response = await fetch(
-  //   `calendar/${format(date, "yyyy")}/${format(date, "M")}/${format(
-  //     date,
-  //     "d"
-  //   )}`,
-  //   {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Accept: "application/json",
-  //     },
-  //   }
-  // );
-  // const data = await response.json();
-  const data = {
-    totalMoney: {
-      가전: -20432,
-      교통: -46486,
-      생필품: -74263,
-      통신: -4200,
-    },
-    totalDetails: {
-      가전: [
-        {
-          type: "가전",
-          way: "현금",
-          id: "20",
-          cost: 20432,
-          memo: "빵 사먹음",
-          ecoList: [
-            {
-              eco: "G",
-              ecoDetail: "중고거래/나눔/기부",
-              userAdd: null,
-            },
-            {
-              eco: "G",
-              ecoDetail: "비건식당 방문",
-              userAdd: null,
-            },
-            {
-              eco: "N",
-              ecoDetail: "사용자 추가",
-              userAdd: "라벨 붙은 음료수 삼",
-            },
-          ],
-          income: false,
-        },
-      ],
-      교통: [
-        {
-          type: "교통",
-          way: "카드",
-          id: "22",
-          cost: 46486,
-          memo: "빵 사먹음",
-          ecoList: [
-            {
-              eco: "G",
-              ecoDetail: "중고거래/나눔/기부",
-              userAdd: null,
-            },
-            {
-              eco: "G",
-              ecoDetail: "비건식당 방문",
-              userAdd: null,
-            },
-            {
-              eco: "N",
-              ecoDetail: "사용자 추가",
-              userAdd: "라벨 붙은 음료수 삼",
-            },
-          ],
-          income: false,
-        },
-      ],
-      생필품: [
-        {
-          type: "생필품",
-          way: "카드",
-          id: "25",
-          cost: 3690,
-          memo: "엽떡 사먹음",
-          ecoList: [
-            {
-              eco: "G",
-              ecoDetail: "다회용기 사용",
-              userAdd: null,
-            },
-            {
-              eco: "G",
-              ecoDetail: "중고거래/나눔/기부",
-              userAdd: null,
-            },
-          ],
-          income: false,
-        },
-        {
-          type: "생필품",
-          way: "현금",
-          id: "26",
-          cost: 70573,
-          memo: "빵 사먹음",
-          ecoList: [
-            {
-              eco: "G",
-              ecoDetail: "중고거래/나눔/기부",
-              userAdd: null,
-            },
-            {
-              eco: "G",
-              ecoDetail: "비건식당 방문",
-              userAdd: null,
-            },
-          ],
-          income: false,
-        },
-      ],
-      통신: [
-        {
-          type: "통신",
-          way: "카드",
-          id: "20",
-          cost: 1400,
-          memo: "new memo",
-          ecoList: [
-            {
-              eco: "G",
-              ecoDetail: "친환경 제품 구매",
-              userAdd: null,
-            },
-            {
-              eco: "N",
-              ecoDetail: "사용자 추가",
-              userAdd: "평생 쓰는 물건 잃어버려서 재구매",
-            },
-            {
-              eco: "G",
-              ecoDetail: "비건식당 방문",
-              userAdd: null,
-            },
-          ],
-          income: false,
-        },
-      ],
-    },
-  };
+  const date = parseISO(day);
+
+  const response = await axios.get(
+    `https://플랜잇.웹.한국:8080/calendar/${format(date, "yyyy")}/${format(
+      date,
+      "M"
+    )}/${format(date, "d")}`
+  );
+  const data = await response.data;
+
   return data;
 };
 
@@ -347,12 +157,6 @@ function CalendarPage() {
           </div>
         </div>
 
-        <div style={{ color: "white" }}>
-          <a href="http://ec2-3-39-87-115.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/kakao">
-            Naver
-          </a>
-        </div>
-
         {isModalOpen && (
           <InfoModal
             className={position}
@@ -385,7 +189,7 @@ function CalendarPage() {
         ) ? (
           <DetailList value={selectedDate} />
         ) : (
-          <div style={{ color: "white" }}>내역 없음</div>
+          <div style={{ color: "white", textAlign: "center" }}>내역 없음</div>
         )}
       </div>
       <Footer activeMenu="calendar">
