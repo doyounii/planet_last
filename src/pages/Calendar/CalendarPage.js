dcc2135b00d81755a9bd5642aef6e058719a17c6
listenyoon <listenyoon@gmail.com>
2022-06-20 23:38:04 +0900

    mod fetch module error

--- src/pages/Calendar/CalendarPage.js
+++ src/pages/Calendar/CalendarPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useQueries, useQueryClient } from "react-query";
 import { format, isSameMonth, subMonths, addMonths, parseISO } from "date-fns";
 import Footer from "../../components/Footer/Footer";
