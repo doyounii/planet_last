bc8af6583a0dd84ab7e82aab970b413822b69d19
leesoyoon <lsyjyh@gmail.com>
2022-08-04 19:56:00 +0900

    statistics

--- src/pages/Statistics/Statistics.js
+++ src/pages/Statistics/Statistics.js
@@ -18,7 +18,7 @@ import { useQueryClient, useQuery } from "react-query";
 
 const fetchData = async (userId, currentMonth) => {
   const response = await axios.get(
-    `https://xn--lj2bx51av9j.xn--yq5b.xn--3e0b707e:8080/api/statistics/${format(date, "yyyy")}/${format(
+    `https://xn--lj2bx51av9j.xn--yq5b.xn--3e0b707e:8080/api/statistics/2022/${format(
       new Date(),
       "M"
     )}/${format(new Date(), "d")}`,
